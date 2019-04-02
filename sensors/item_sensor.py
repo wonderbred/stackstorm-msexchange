6ed51aef6a00f127602304567737176e82390a84
Brett Canter <brettcanter@gmail.com>
2019-04-01 22:30:56 -0700

    More updates to improve datetime/EWSDateTime handling

--- sensors/item_sensor.py
+++ sensors/item_sensor.py
@@ -1,22 +1,23 @@
 from datetime import datetime
-import time
 
+from exchangelib import Account, Configuration, DELEGATE, EWSDateTime, \
+    EWSTimeZone, ServiceAccount
 from st2reactor.sensor.base import PollingSensor
-from exchangelib import Account, ServiceAccount, Configuration, DELEGATE, EWSDateTime, EWSTimeZone
 
 
 class ItemSensor(PollingSensor):
     def __init__(self, sensor_service, config):
-        super(ItemSensor, self).__init__(sensor_service=sensor_service, config=config)
-        self._logger = self.sensor_service.get_logger(name=self.__class__.__name__)
+        super(ItemSensor, self).__init__(sensor_service=sensor_service,
+                                         config=config)
+        self._logger = self.sensor_service.get_logger(
+            name=self.__class__.__name__)
         self._stop = False
         self._store_key = 'exchange.item_sensor_date_str'
         self._timezone = EWSTimeZone.timezone(config['timezone'])
-        self._credentials = ServiceAccount(
-            username=config['username'],
-            password=config['password'])
+        self._credentials = ServiceAccount(username=config['username'],
+                                           password=config['password'])
         self.primary_smtp_address = config['primary_smtp_address']
-        self.sensor_folder = config['sensor_folder']
+        self.sensor_folder = config.get('sensor_folder')
         try:
             self.server = config['server']
             self.autodiscover = False if self.server is not None else True
@@ -39,26 +40,34 @@ class ItemSensor(PollingSensor):
                 config=ms_config,
                 autodiscover=False,
                 access_type=DELEGATE)
+        self.stored_date = self._get_last_date()
+        if not self.stored_date:
+            self.stored_date = self._timezone.localize(
+                EWSDateTime.from_datetime(datetime.now()))
 
     def poll(self):
-        stored_date = self._get_last_date()
-        if not stored_date:
-            stored_date = datetime.now()
-        start_date = self._timezone.localize(EWSDateTime.from_datetime(stored_date))
-        if self.sensor_folder.lower() == 'inbox':
-            target = self.account.inbox
-        else:
+        start_date = self._get_last_date()
+        if not start_date:
+            start_date = self.stored_date
+        if self.sensor_folder:
             target = self.account.inbox.parent / self.sensor_folder
-        items = target.filter(is_read=False).filter(datetime_received__gt=start_date)
+        else:
+            target = self.account.inbox
+        items = target.filter(is_read=False).filter(
+            datetime_received__gt=start_date)
 
         self._logger.info("Found {0} items".format(items.count()))
-        for payload in items.values('item_id', 'subject', 'body', 'datetime_received'):
-            self._logger.info("Sending trigger for item '{0}'.".format(payload['subject']))
-            self._sensor_service.dispatch(trigger='exchange_new_item', payload=payload)
+        for payload in items.values('item_id', 'subject', 'body', 'text_body',
+                                    'datetime_received'):
+            self._logger.info(
+                "Sending trigger for item '{0}'.".format(payload['subject']))
+            self._sensor_service.dispatch(trigger='exchange_new_item',
+                                          payload=payload)
             self._set_last_date(payload['datetime_received'])
 
     def cleanup(self):
-        # This is called when the st2 system goes down. You can perform cleanup operations like
+        # This is called when the st2 system goes down. You can perform
+        # cleanup operations like
         # closing the connections to external system here.
         pass
 
@@ -78,9 +87,9 @@ class ItemSensor(PollingSensor):
         self._last_date = self._sensor_service.get_value(name=self._store_key)
         if self._last_date is None:
             return None
-        return time.strptime(self._last_date, '%Y-%m-%dT%H:%M:%S')
+        return EWSDateTime.from_string(self._last_date)
 
     def _set_last_date(self, last_date):
-        self._last_date = time.strftime('%Y-%m-%dT%H:%M:%S', last_date)
+        self._last_date = last_date.ewsformat()
         self._sensor_service.set_value(name=self._store_key,
                                        value=self._last_date)
