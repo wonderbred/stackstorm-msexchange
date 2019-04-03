cf337cbf3bf7cc121ec9af9b5d931c0690751010
Brett Canter <brettcanter@gmail.com>
2019-04-02 23:18:36 -0700

    Update for new exchangelib v1.11+ folder management

--- actions/list_folders.py
+++ actions/list_folders.py
@@ -4,9 +4,14 @@ from base import folder_to_dict
 
 class ListFoldersAction(BaseExchangeAction):
     def run(self, root=None):
-        if root:
-            folders = self.account.root.glob('*/{}'.format(root)).folders
+        if root is not None:
+            result = self.account.root.glob('*/{}'.format(root)).folders
+            try:
+                parent = result[0]
+            except IndexError:
+                self.logger.error('Root folder not found: {}'.format(root))
+                return False
         else:
-            folders = self.account.inbox.parent.children.folders
+            parent = self.account.inbox.parent.children.folders
 
-        return [folder_to_dict(folder) for folder in folders]
+        return (True, [folder_to_dict(folder) for folder in parent])
