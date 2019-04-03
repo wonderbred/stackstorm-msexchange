78f69d0c2838a4b9907e4eac219ba7603bf3a55d
Brett Canter <brettcanter@gmail.com>
2019-04-03 15:44:21 -0700

    New action move_items

---
+++ actions/move_items.py
@@ -0,0 +1,28 @@
+from exchangelib.errors import ErrorItemNotFound
+
+from lib.action import BaseExchangeAction
+
+
+class MoveItemsAction(BaseExchangeAction):
+    def run(self, item_ids, from_folder, to_folder, mark_read):
+        from_folder = self.account.inbox.parent / from_folder
+        to_folder = self.account.inbox.parent / to_folder
+        i = 0
+        for item_id in item_ids:
+            item = from_folder.get(id=item_id)
+            try:
+                assert type(item) != ErrorItemNotFound
+                self.logger.debug('Item found')
+            except AssertionError:
+                return (False, 'Item(id="{}") not found'.format(item_id))
+            if mark_read:
+                self.mark_item_read(item)
+                self.logger.debug('Item(id="{}") marked read'.format(item_id))
+            item.move(to_folder=to_folder)
+            self.logger.info(
+                'Item(subject="{}") moved to "{}"'.format(item.subject,
+                                                          to_folder))
+            i += 1
+
+        return (True, '{} items moved from "{}" to "{}"'.format(i, from_folder,
+                                                                to_folder))
