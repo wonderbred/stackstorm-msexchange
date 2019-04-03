b29378c759aae3d3cf5837927162be5ae45ee7ef
Brett Canter <brettcanter@gmail.com>
2019-04-02 22:46:33 -0700

    Update for new exchangelib v1.11+ folder management

--- actions/get_folder.py
+++ actions/get_folder.py
@@ -4,6 +4,10 @@ from base import folder_to_dict
 
 class GetFolderAction(BaseExchangeAction):
     def run(self, folder_name):
-        result = self.account.root.get_folder_by_name(folder_name)
-
-        return folder_to_dict(result)
+        result = self.account.root.glob('*/{}'.format(folder_name)).folders
+        try:
+            folder = result[0]
+            return (True, folder_to_dict(folder))
+        except IndexError:
+            self.logger.error('Folder not found: {}'.format(folder_name))
+            return False
